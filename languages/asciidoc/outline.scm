9b0e58831b57c755afe3f66e0d9f263aadbf4b34
Dunya Kirkali <dunyakirkali@gmail.com>
2026-01-06 14:28:14 +0100

    feat: add outline (#20)

---
+++ languages/asciidoc/outline.scm
@@ -0,0 +1,19 @@
+;; Document title
+(document_title) @item @name
+(#set! kind "file")
+
+;; Section titles (levels 1–5)
+(title1) @item @name
+(#set! kind "section")
+
+(title2) @item @name
+(#set! kind "section")
+
+(title3) @item @name
+(#set! kind "section")
+
+(title4) @item @name
+(#set! kind "section")
+
+(title5) @item @name
+(#set! kind "section")
